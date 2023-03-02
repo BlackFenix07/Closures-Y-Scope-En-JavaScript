8b437a7a3d74e47dbcfc83eef1a7aa3220438c43
Andrés Zapata <andresmauricio030400@gmail.com>
2023-03-01 19:51:03 -0500

    Tipos de Scope en JavaScript

---
+++ src/scope/re.js
@@ -0,0 +1,27 @@
+var firstName; // Declaración.
+firstName = "Andrés"; // Asignación.
+console.log(firstName);
+
+var secondName = "Serkan"; // Declaración y asignación.
+var secondName = "Mauricio"; // Redeclaración.
+console.log(secondName);
+
+var lastName = "Bolat"; // Declaración y asignación.
+lastName = "Zapata"; // Reasignación.
+console.log(lastName);
+
+let fruit = "Apple"; // Declaración y asignación.
+fruit = "Kiwi"; // Reasignación.
+console.log(fruit);
+// let fruit = "Banana"; // Redeclaración.
+// console.log(fruit);
+
+const animal = "Dog"; // Declaración y asignación.
+// const animal = "Cat"; // Redeclaración.
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+vehicles.pop();
+console.log(vehicles);
